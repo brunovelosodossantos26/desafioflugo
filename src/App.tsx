44c3efb6e04cdd1f23611ed93226bb8ff670068d
Bruno Veloso <brvelosoo@gmail.com>
2025-08-21 16:37:37 -0300

    Sidebar

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="h-screen bg-white">
+      <div className="h-[100%] flex">
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
